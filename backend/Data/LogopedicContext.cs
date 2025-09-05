27fa2e0207fa7b87c96155c1c8a1f9b490c95e39
olafmyszak <myszakolaf@gmail.com>
2025-09-05 20:22:43 +0200

    Refactor db configs into separate files

--- backend/Data/LogopedicContext.cs
+++ backend/Data/LogopedicContext.cs
@@ -20,49 +20,6 @@ public class LogopedicContext(DbContextOptions<LogopedicContext> options)
         modelBuilder.HasPostgresExtension("unaccent");
         modelBuilder.HasPostgresExtension("pg_trgm");
 
-        modelBuilder.Entity<Therapist>(entityBuilder =>
-        {
-            entityBuilder.ToTable("Therapists", "public")
-                .HasMany(t => t.Patients)
-                .WithOne(p => p.Therapist)
-                .HasForeignKey(p => p.TherapistId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        modelBuilder.Entity<Patient>(entityBuilder =>
-        {
-            entityBuilder.ToTable("Patients", "public")
-                .HasMany(p => p.Appointments)
-                .WithOne(a => a.Patient)
-                .HasForeignKey(a => a.PatientId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            entityBuilder.Property(p => p.SearchText)
-                .HasComputedColumnSql(
-                    "lower(unaccent(coalesce(FullName,'') || ' ' || coalesce(ContactInfo,''))",
-                    true);
-
-            entityBuilder.HasIndex(p => p.FullName);
-            entityBuilder.HasIndex(p => p.ContactInfo);
-        });
-
-        modelBuilder.Entity<Appointment>(entityBuilder =>
-        {
-            entityBuilder.ToTable("Appointments", "public")
-                .HasOne(a => a.Therapist)
-                .WithMany(t => t.Appointments)
-                .HasForeignKey(a => a.TherapistId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            entityBuilder.HasOne(a => a.Patient)
-                .WithMany(p => p.Appointments)
-                .HasForeignKey(a => a.PatientId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            entityBuilder.HasIndex(a => a.StartTime);
-            entityBuilder.HasIndex(a => a.DurationInMinutes);
-            entityBuilder.HasIndex(a => a.Type);
-            entityBuilder.HasIndex(a => a.Status);
-        });
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LogopedicContext).Assembly);
     }
 }
